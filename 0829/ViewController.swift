831ef7e7319c6bfe234de87b473ad8a4806f3444
cherie <sjeongab@connect.ust.hk>
2020-08-29 10:41:39 +0800

    Initial Commit

---
+++ 0829/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  0829
+//
+//  Created by Cherie Jeong on 29/8/2020.
+//  Copyright © 2020 Cherie Jeong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 0829Tests/_829Tests.swift
@@ -0,0 +1,34 @@
+//
+//  _829Tests.swift
+//  0829Tests
+//
+//  Created by Cherie Jeong on 29/8/2020.
+//  Copyright © 2020 Cherie Jeong. All rights reserved.
+//
+
+import XCTest
+@testable import _829
+
+class _829Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
